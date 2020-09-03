b002d3c1105605d8de89353eb0fd6ed319dd8414
Samuel Nishimwe <sam.nishimwe@yahoo.com>
2020-09-03 12:35:05 +0200

    Add Continuous Integration & Qaulity Assurance pipeline tr rqq11y (#1)
    * Ch: add node_module packages
    
    * ch(ci-qa): configure mocha
    
    * ch(ci-qa): add travis yml
    
    * ch(ci-qa): Add start script
    
    * ch(ci-qa): Move test dir from source files
    
    * ch(ci-qa): add types/express to dependencies
    
    * ch(ci-qa): Specify which files to compile
    
    * ch(ci-qa): use env port in production
    
    * ch(ci-qa): Add coverage reporting

---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import assert from 'assert'
+describe('Test setup', () => {
+    it('should find the correct artist', () => {
+        let artist: string = 'Illenium'
+        assert(typeof artist === "string", 'you don\'t know ts, do you?')
+    })
+})
